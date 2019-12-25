dd8501c0dcb64aaf866b299e5ebe34e28aabda44
wcss <wcss2020@gmail.com>
2019-12-25 16:17:39 +0800

    ...

--- Code/PublicManager/PublicManager/Modules/Module_A/DataManager/Forms/AddOrUpdateProjectForm.cs
+++ Code/PublicManager/PublicManager/Modules/Module_A/DataManager/Forms/AddOrUpdateProjectForm.cs
@@ -188,28 +188,4 @@ namespace PublicManager.Modules.Module_A.Manager.Forms
             DialogResult = System.Windows.Forms.DialogResult.Cancel;
         }
     }
-
-    /// <summary>
-    /// ComboBoxObject
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class ComboBoxObject<T>
-    {
-        public ComboBoxObject() { }
-
-        public ComboBoxObject(string text,T tag)
-        {
-            this.Text = text;
-            this.Tag = tag;
-        }
-
-        public string Text { get; set; }
-
-        public T Tag { get; set; }
-
-        public override string ToString()
-        {
-            return Text;
-        }
-    }
 }
