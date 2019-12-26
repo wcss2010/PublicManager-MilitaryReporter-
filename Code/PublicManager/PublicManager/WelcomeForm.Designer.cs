aeef570cce1f845312b064b006cf41de9b714e64
wcss <wcss2020@gmail.com>
2019-12-26 09:51:07 +0800

    ...

---
+++ Code/PublicManager/PublicManager/WelcomeForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PublicManager
+{
+    partial class WelcomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WelcomeForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnToA = new System.Windows.Forms.Button();
+            this.btnToB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(816, 329);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnToA
+            // 
+            this.btnToA.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnToA.Location = new System.Drawing.Point(107, 371);
+            this.btnToA.Name = "btnToA";
+            this.btnToA.Size = new System.Drawing.Size(195, 77);
+            this.btnToA.TabIndex = 1;
+            this.btnToA.Text = "其它地区入口";
+            this.btnToA.UseVisualStyleBackColor = true;
+            this.btnToA.Click += new System.EventHandler(this.btnToA_Click);
+            // 
+            // btnToB
+            // 
+            this.btnToB.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnToB.Location = new System.Drawing.Point(529, 371);
+            this.btnToB.Name = "btnToB";
+            this.btnToB.Size = new System.Drawing.Size(195, 77);
+            this.btnToB.TabIndex = 1;
+            this.btnToB.Text = "军委机构入口";
+            this.btnToB.UseVisualStyleBackColor = true;
+            this.btnToB.Click += new System.EventHandler(this.btnToB_Click);
+            // 
+            // WelcomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 483);
+            this.Controls.Add(this.btnToB);
+            this.Controls.Add(this.btnToA);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WelcomeForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "汇总系统";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnToA;
+        private System.Windows.Forms.Button btnToB;
+    }
+}
---
+++ Code/PublicManager/PublicManager/WelcomeForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PublicManager
+{
+    public partial class WelcomeForm : Form
+    {
+        public WelcomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnToB_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
