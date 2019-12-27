1cac429ac7d4b340bd36740def6497eb99d79ac6
wcss <wcss2020@gmail.com>
2019-12-27 15:59:29 +0800

    ...

--- Code/PublicManager/PublicManager/Modules/Module_A/ModuleMainForm.cs
+++ Code/PublicManager/PublicManager/Modules/Module_A/ModuleMainForm.cs
@@ -10,15 +10,18 @@ namespace PublicManager.Modules.Module_A
 {
     public class ModuleMainForm : BaseModuleMainForm
     {
-        public ModuleMainForm()
-            : base()
+        private List<string> dutyUnitToProfessonLinks;
+        /// <summary>
+        /// 责任单位(有类别的)
+        /// </summary>
+        public List<string> DutyUnitToProfessonLinks
         {
-            LocalUnit lu = ConnectionManager.Context.table("LocalUnit").select("*").getItem<LocalUnit>(new LocalUnit());
-            Text = "其它地区(" + lu.LocalUnitName + ")汇总系统";
+            get { return dutyUnitToProfessonLinks; }
         }
 
         private DevExpress.XtraTreeList.TreeList treeListObj;
         private DevExpress.XtraNavBar.NavBarGroup firstPage;
+
         protected override void initMenus()
         {
             base.initMenus();
@@ -44,7 +47,43 @@ namespace PublicManager.Modules.Module_A
         {
             base.initUI();
 
-            treeListObj = buildTreeControl(new string[] { string.Empty }, new string[] { "数据包汇总", "字典维护", "数据管理", "数据导出" });
+            //加载责任单位与专业类型映射选项
+            if (MainConfig.Config.ObjectDict.ContainsKey("责任单位与专业类别映射"))
+            {
+                try
+                {
+                    dutyUnitToProfessonLinks = new List<string>();
+                    Newtonsoft.Json.Linq.JArray teams = (Newtonsoft.Json.Linq.JArray)MainConfig.Config.ObjectDict["责任单位与专业类别映射"];
+                    foreach (string s in teams)
+                    {
+                        dutyUnitToProfessonLinks.Add(s);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.ToString());
+                }
+            }
+
+            LocalUnit lu = ConnectionManager.Context.table("LocalUnit").select("*").getItem<LocalUnit>(new LocalUnit());
+            Text = "其它地区(" + lu.LocalUnitName + ")汇总系统";
+            
+            if (string.IsNullOrEmpty(lu.LocalUnitID))
+            {
+                treeListObj = buildTreeControl(new string[] { string.Empty }, new string[] { "数据包汇总", "字典维护", "数据管理", "数据导出" });
+            }
+            else
+            {
+                if (DutyUnitToProfessonLinks.Contains(lu.LocalUnitName))
+                {
+                    treeListObj = buildTreeControl(new string[] { string.Empty }, new string[] { "数据包汇总", "字典维护", "数据管理", "数据导出" });
+                }
+                else
+                {
+                    treeListObj = buildTreeControl(new string[] { string.Empty }, new string[] { "数据包汇总", "数据管理", "数据导出" });
+                }
+            }
+
             treeListObj.MouseClick += treeListObj_MouseClick;
             firstPage = appendPage("数据汇总", PublicManager.Properties.Resources.Mail_32x32);
             firstPage.ControlContainer.Controls.Add(treeListObj);
