08741ce650cb6462aa85814402596a19085e9358
wcss <wcss2020@gmail.com>
2020-01-09 17:01:29 +0800

    ...

--- Code/PublicManager/PublicManager/Modules/Module_B/DictManager/MainView.cs
+++ Code/PublicManager/PublicManager/Modules/Module_B/DictManager/MainView.cs
@@ -116,6 +116,7 @@ namespace PublicManager.Modules.Module_B.DictManager
                 cells.Add(pfo.ProfessionNum);
                 cells.Add(pfo.ProfessionCategory);
                 cells.Add(pfo.ProfessionName);
+                cells.Add(pfo.IsAcceptModify == "true");
 
                 int rowIndex = dgvCatalogs.Rows.Add(cells.ToArray());
                 dgvCatalogs.Rows[rowIndex].Tag = pfo;
